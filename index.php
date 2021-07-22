571bb686f2ab64472bdbb9c1119e287aee367e27
ichinosee <67300586+ichinosee@users.noreply.github.com>
2021-07-22 15:57:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php require_once('data.php') ?>
+<?php 
+require_once('data.php');
+require_once('menu.php');
+?>
 
 <!DOCTYPE html>
 <html>
@@ -11,12 +14,22 @@
 <body>
   <div class="menu-wrapper container">
     <h1 class="logo">Café Progate</h1>
+    <h3>メニュー<?php echo Menu::getCount() ?>品</h3>
     <form method="post" action="confirm.php">
       <div class="menu-items">
         <?php foreach ($menus as $menu): ?>
           <div class="menu-item">
             <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
             <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+            <?php if ($menu instanceof Drink): ?>
+              <p class="menu-item-type"><?php echo $menu->getType() ?></p>
+            <?php else: ?>
+              <!-- for文を用いて、spicinessプロパティの数だけ繰り返し処理を行ってください -->
+              <?php for($i = 0; $i<$menu->getSpiciness(); $i++): ?>
+              <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class = 'icon-spiciness'>
+              <?php endfor ?>
+              
+            <?php endif ?>
             <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
             <input type="text" value="0" name="<?php echo $menu->getName() ?>">
             <span>個</span>
